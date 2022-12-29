d65bae3baef17defa4f433fe4a690f6287c4f8cf
JM O'Brien <>
2022-12-28 22:26:17 -0800

    First commit

---
+++ src/Section2_IntroToJava/KeyboardInput.java
@@ -0,0 +1,21 @@
+package Section2_IntroToJava;
+
+import java.util.Scanner;
+
+/**
+ * @author john-michael.obrien
+ * @since 12/20/22
+ */
+public class KeyboardInput {
+    public static void main (String args[]) {
+        Scanner keyboard = new Scanner(System.in);
+        double michaelDouble;
+        double result = 0;
+
+        System.out.println("Please give me a double yo:");
+        michaelDouble = keyboard.nextDouble();
+        result = michaelDouble *= 2;
+        System.out.println("Double has been updated to: " + result + '\n');
+        System.out.print("Test is this on a skipped line with no additional space after?");
+    }
+}
