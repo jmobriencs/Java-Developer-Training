d77474deb997cae0df23dad9804214c5e8ddf2a0
JM O'Brien <>
2023-01-21 13:12:02 -0800

    Completed section 6 pt 2 method overloading

---
+++ src/Section6_Methods/Lessons/MethodsPt2_Overloading.java
@@ -0,0 +1,46 @@
+package Section6_Methods.Lessons;
+
+/**
+ * @author john-michael.obrien
+ * @since 1/21/23
+ *
+ * Pass-by-value:
+ *  A copy of the data being passed in is created
+ *  Changes to the copy do not affect the original
+ *
+ * Pass-by-reference:
+ *  A copy is NOT created
+ *  The address is passed in
+ *  Any changes to the parameter passed in affects the argument that
+ *  was passed in when the method was called
+ *
+ * Method overloading:
+ *  Same method name, different parameters
+ */
+public class MethodsPt2_Overloading {
+    public static void main(String[] args) {
+        System.out.println(getResult(4));
+        System.out.println(getResult(4, 8));
+        System.out.println(getResult(4, "8"));
+        System.out.println(getResult("JM", "O'Brien"));
+    }
+
+    /** Example of overloaded methods */
+
+    public static int getResult(int num) {
+        return num * 2;
+    }
+
+    public static int getResult(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int getResult(int num, String value) {
+        return num * Integer.parseInt(value);
+    }
+
+    // Challenge: return a different data type using the same method
+    public static String getResult(String a, String b) {
+        return a + " " + b;
+    }
+}
