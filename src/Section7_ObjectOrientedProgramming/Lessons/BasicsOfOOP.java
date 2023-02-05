b5f5ee048eb0b482d984ca41e517b09d52224620
JM O'Brien <>
2023-02-05 02:09:10 -0800

    Basics of OOP

---
+++ src/Section7_ObjectOrientedProgramming/Lessons/BasicsOfOOP.java
@@ -0,0 +1,18 @@
+package Section7_ObjectOrientedProgramming.Lessons;
+
+/**
+ * @author john-michael.obrien
+ * @since 2/4/23
+ *
+ *
+ */
+public class BasicsOfOOP {
+    public static void main(String[] args) {
+        /** OOP
+         *  An object represents an entity that has both behaviors and data
+         *  This forms encapsulation
+         *  Three primary principles of OOP:
+         *      Encapsulation, inheritance, and polymorphism
+         */
+    }
+}
