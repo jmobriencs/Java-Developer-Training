5ed2629af6b9272b0f17da118e31f3d700cc50e5
JM O'Brien <jmobriencs@gmail.com>
2023-04-10 09:56:22 -0700

    Delete unused import

--- src/linkedinlearning/javaobjectsandapis/section7_exceptionhandling/RethrowingExceptions.java
+++ src/linkedinlearning/javaobjectsandapis/section7_exceptionhandling/RethrowingExceptions.java
@@ -3,7 +3,6 @@ package linkedinlearning.javaobjectsandapis.section7_exceptionhandling;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
